dd343faa907273d24dffa0523fa1ee867b7e8db3
Deepthy Nair <dpthynair@gmail.com>
2020-04-27 17:32:19 +0530

    Combining Reducers

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,9 +1,25 @@
-import { createStore } from 'redux';
-import { Reducer ,initialState } from './reducer';
+import { createStore, combineReducers } from 'redux';
+//import { Reducer ,initialState } from './reducer';
+import { Dishes } from './dishes';
+import { Comments } from './comments';
+import { Promotions } from './promotions';
+import { Leaders } from './leaders';
 
 
-export const configureStore = () => {
-    const store = createStore(Reducer, initialState);
+// export const configureStore = () => {
+//     const store = createStore(Reducer, initialState);
 
-    return store;
+//     return store;
+// }
+
+export const configureStore = ()=> {
+    const store = createStore
+    (combineReducers({
+        dishes: Dishes,
+        comments: Comments,
+        promotions: Promotions,
+        leaders: Leaders
+    })
+    ); 
+    return store; 
 }
